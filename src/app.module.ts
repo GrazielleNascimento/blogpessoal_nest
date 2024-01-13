113eaaa88a0b98cebec4cae9f88801fa9998a10f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-13 18:12:59 -0300

    fix db name

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'; //Importamos o pacote TypeORMMo
       port: 3306, //3306 - Porta padrão do MySQL
       username: 'root',
       password: 'root',
-      database: 'db_blogpessoa_nest', //A propriedade database define o nome do Banco de dados que foi criado no MySQL
+      database: 'db_blogpessoal_nest', //A propriedade database define o nome do Banco de dados que foi criado no MySQL
       entities: [],
       //A propriedade synchronize definida com true indica que as tabelas do Banco de dados serão criadas/atualizadas automaticamente em cada inicialização da aplicação.
       synchronize: true,
