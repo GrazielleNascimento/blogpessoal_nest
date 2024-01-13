0bcb5542cc75adc598918d8dbc7375606eb01af2
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-13 17:01:09 -0300

    implementa configuracao do banco de dados MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; //Importamos o pacote TypeORMModule, que foi instalado anteriormente
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({//metodo forRoot() configura a conexão com o banco de dados
+      type: 'mysql', // A propriedade type define o tipo do Banco de dados (MySQL)
+      host: 'localhost',// define o endereço do servidor onde Banco de dados está hospedado
+      port: 3306, //3306 - Porta padrão do MySQL
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoa_nest', //A propriedade database define o nome do Banco de dados que foi criado no MySQL
+      entities: [],
+      //A propriedade synchronize definida com true indica que as tabelas do Banco de dados serão criadas/atualizadas automaticamente em cada inicialização da aplicação.
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
-})
+})  
 export class AppModule {}
