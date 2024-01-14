cf901ce16646df0274c4917c5e59abdee7aea890
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-14 08:54:34 -0300

    add relacao bidirecional 1:N

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,6 +1,6 @@
-
 import { IsNotEmpty } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Postagem } from '../../postagem/entities/postagem.entity';
 
 @Entity({ name: 'tb_temas' })
 export class Tema {
@@ -11,4 +11,8 @@ export class Tema {
   @Column({ length: 255, nullable: false }) //O decorator @Column define que o atributo será inserido na estrutura da Tabela, ou seja, se você não declarar o decorator @Column (exceto nos atributos Chave Primária e Timestamp), o atributo não será inserido na estrutura da tabela.
   descricao: string;
 
+  //RELACIONAMENTO
+  //O decorator @OneToMany indica que a Classe Tema será o lado 1:N
+  @OneToMany(() => Postagem, (postagem) => postagem.tema)
+  postagem: Postagem[];
 }
