d3a4d11e1c83122630b1da533b2c1f7f5fe0933c
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-13 22:55:30 -0300

    add metodo create na controller

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { PostagemService } from '../services/postagem.service';
 import { Postagem } from '../entities/postagem.entity';
-import { ILike, Repository } from 'typeorm';
 
 @Controller('/postagens')
 export class PostagemController {
-
   //Cria um Construtor, que receberá as Injeções de Dependência.
   // Como precisaremos ter acesso aos Métodos da Classe PostagemService, foi injetado um Objeto desta Classe dentro do Construtor.
   constructor(private readonly postagemService: PostagemService) {}
@@ -22,16 +29,25 @@ export class PostagemController {
 
   //@Param('id'): Este Decorator insere o valor enviado na variável de caminho id (:id),
   // no parâmetro do Método findById( @Param('id') id: number );
-  //ParseIntPipe: Converte o valor da variável de caminho id (inicialmente uma string) em um numero. Caso não seja possível converter (o usuário digitou algo diferente de um numero), ele retorna uma mensagem de erro 
+  //ParseIntPipe: Converte o valor da variável de caminho id (inicialmente uma string) em um numero. Caso não seja possível converter (o usuário digitou algo diferente de um numero), ele retorna uma mensagem de erro
   findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
-    return this.postagemService.findById(id)
+    return this.postagemService.findById(id);
   }
 
-  @Get('/titulo/:titulo')//http://localhost:4000/postagens/titulo/palavra
+  @Get('/titulo/:titulo') //http://localhost:4000/postagens/titulo/palavra
   @HttpCode(HttpStatus.OK)
 
   //@Param('titulo'): Este decorator insere o valor enviado na variável de caminho titulo (:titulo), no parâmetro do Método findByTitulo(@Param('titulo') titulo: string);
-  findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
-    return this.postagemService.findByTitulo(titulo)
+  findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findByTitulo(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+
+  //@Body(): Este decorator insere o Objeto postagem enviado no corpo da Requisição,
+  //no Objeto postagem (parâmetro) do Método create(@Body() postagem: Postagem);
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
   }
 }
