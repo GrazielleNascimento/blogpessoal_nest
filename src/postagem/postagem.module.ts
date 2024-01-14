28a1a18746fe0eaa8d26d1d2820c0d46e68e78fd
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-14 12:55:37 -0300

    Registra o Módulo TemaModule e a Classe TemaService no Módulo PostagemModule

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -3,11 +3,13 @@ import { Postagem } from './entities/postagem.entity'; //Importamos a Classe Pos
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostagemService } from './services/postagem.service';
 import { PostagemController } from './controllers/postagem.controller';
+import { TemaService } from '../tema/services/tema.service';
+import { TemaModule } from '../tema/tema.module';
 
 // decorator @Module indica que esta classe será a Classe principal do Módulo, ou seja, nela registraremos todas as Classes (Entidade, Service e Controller) criadas dentro do Módulo.
 @Module({
-    imports: [TypeOrmModule.forFeature([Postagem])],//: No array imports vamos importar todas as Classes Entidade (Model) do Módulo, através do Método forFeature, da Classe TypeOrmModule. Observe que importamos a Classe Entidade Postagem.
-    providers:[PostagemService],//importacao das classes de Serviço do Modulo
+    imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],//: No array imports vamos importar todas as Classes Entidade (Model) do Módulo, através do Método forFeature, da Classe TypeOrmModule. Observe que importamos a Classe Entidade Postagem.
+    providers:[PostagemService, TemaService],//importacao das classes de Serviço do Modulo
     controllers: [PostagemController], //importacao das classes Controladoradoras do Modulo
     exports: [TypeOrmModule]// adiciona classes que precisam estar disponiveis p outros Módulos
 //No array exports vamos importar todas as Classes do Módulo que precisam estar disponíveis para outros Módulos. Como teremos que registrar a Classe Postagem no Módulo principal da aplicação (AppModule), vamos inserir no array a Classe do TypeORM utilizada para registrar a Classe Postagem (TypeOrmModule).
